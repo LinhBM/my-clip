64a474a1fd9115e9a00e43e8512cd0a16bf953b4
linhbm <linhbm@outlook.com>
2022-09-23 13:47:31 +0700

    tao time tren login

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,13 @@
 import logo from "./logo.svg";
 
 function Login() {
-  return <div></div>;
+  return (
+    <div className="relative w-[375px] h-[812px] bg-[#000000] mx-auto">
+      <span className="absolute text-[#ffffff] text-[15px] leading-[18px] w-[54px] h-[18px] left-[21px] top-[14px] text-center tracking-[-0.3px] font-[sf-pro-display] font-medium">
+        9:41
+      </span>
+    </div>
+  );
 }
 
 export default Login;
