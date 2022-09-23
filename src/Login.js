ac47b5b8c8176c174b4bf1cad7bf1138b0dfbe9f
linhbm <linhbm@outlook.com>
2022-09-23 15:44:09 +0700

    vector

--- src/Login.js
+++ src/Login.js
@@ -2,10 +2,41 @@ import logo from "./logo.svg";
 
 function Login() {
   return (
-    <div className="relative w-[375px] h-[812px] bg-[#000000] mx-auto">
-      <span className="absolute text-[#ffffff] text-[15px] leading-[18px] w-[54px] h-[18px] left-[21px] top-[14px] text-center tracking-[-0.3px] font-[sf-pro-display] font-medium">
-        9:41
-      </span>
+    <div className="absolute w-[480px] h-[716px] left-[720px] top-[182px] bg-[#000000] border border-solid border-[#141414] rounded-2xl">
+      <div className="absolute w-6 h-6 left-[440px] top-4">
+        <svg
+          width="14"
+          height="14"
+          viewBox="0 0 14 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute top-1/4 right-1/4 bottom-1/4 left-1/4 border-solid border-[#B0B0B8]"
+        >
+          <path
+            d="M1 1L13 13"
+            stroke="#B0B0B8"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <svg
+          width="14"
+          height="14"
+          viewBox="0 0 14 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute top-1/4 right-1/4 bottom-1/4 left-1/4 border-solid border-[#B0B0B8]"
+        >
+          <path
+            d="M13 1L1 13"
+            stroke="#B0B0B8"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
